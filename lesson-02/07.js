a4a2854a55a8ba57b6168f4ccc2904a200f4698e
AlexeyFedyaev <alexey.fedyaev1@gmail.com>
2023-07-31 12:35:59 +0300

    add married status

---
+++ lesson-02/07.js
@@ -0,0 +1,18 @@
+let passporNottMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let passportMarried = {
+  ...passporNottMarried,
+    address: {
+      ...passporNottMarried.address
+    }
+};
+passportMarried.married = true;
+console.log(passporNottMarried); 
+console.log(passportMarried);  
