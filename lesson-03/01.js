0e4ca9dc960874b28185f18e68240f83ecfa2674
AlexeyFedyaev <alexey.fedyaev1@gmail.com>
2023-08-17 20:26:31 +0300

    add filter

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,10 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый"];
+
+let germanFlag = colors.filter(function createColorString(color) {
+  if (color === "черный" || color === "красный" || color === "желтый") {
+    return true;
+  }
+  return false;
+});
+
+console.log(germanFlag);
