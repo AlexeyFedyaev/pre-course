85dbb36a2d5e39b8222d3088fe821e9cc5ebcc97
AlexeyFedyaev <alexey.fedyaev1@gmail.com>
2023-08-20 15:11:16 +0300

    changemore

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,8 +1,8 @@
-​const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
 let orangeIndex = words.indexOf("апельсин");
-if(orangeIndex === -1) {
-  console.log("«Придется поискать в другом магазине...");
+if(orangeIndex >= 0) {
+  console.log("Ура! нашел");
 }
-  else { 
-    console.log("Ура! нашел");
+  else {
+    console.log("«Придется поискать в другом магазине...");
   }
